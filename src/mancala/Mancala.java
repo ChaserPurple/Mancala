14c501a57bb808879ed43e36d966c0ff3e2168b1
chickens4473 <44711686+chickens4473@users.noreply.github.com>
2018-11-09 12:43:41 -0800

    Merge branch 'master' of https://github.com/ChaserPurple/Mancala

--- src/mancala/Mancala.java
+++ src/mancala/Mancala.java
@@ -128,11 +128,47 @@ public class Mancala extends JFrame implements Runnable {
         g.setColor(Color.red);
         g.drawPolyline(x, y, 5);
 
+        if(!begun){
+            Drawing.drawScreen();
+        }
+        
         if (animateFirstTime) {
             gOld.drawImage(image, 0, 0, null);
             return;
         }
 
+        if(begun){
+            Scoops.Draw(g);
+            g.drawImage(Grass,Window.getX(0),Window.getY(0),Window.getWidth2(),Window.getHeight2(),this);
+            g.drawImage(Board,Window.getX(0)-40,Window.getY(2)+Window.getHeight2()/4,
+                    Window.getWidth2()+70,Window.getHeight2()/2,this);
+            //animal.Draw(g);
+
+
+                if (Player.getCurrentPlayer()== Player.getPlayer1())
+                {
+                g.setColor(Color.white);
+                g.setFont(new Font("Arial",Font.BOLD,25));
+                g.drawString("Player1's turn",250,60);
+                }
+                else {
+                g.setColor(Color.black);
+                g.setFont(new Font("Arial",Font.BOLD,25));
+                g.drawString("Player2's turn" ,250,60);   
+                }
+
+                g.setColor(new Color(98,53,18));
+                g.fillRect(Window.getX(230),Window.getY(75)+Window.getHeight2()/3,
+                    Window.getWidth2()/2,Window.getHeight2()/9);
+                g.setColor(Color.black);
+                g.setFont(new Font("Arial",Font.BOLD,35));
+                g.drawString("ANIMACALA" ,350,350);   
+
+            if (win)
+            {
+                g.setColor(Color.blue);
+                g.setFont(new Font("Arial",Font.PLAIN,45));
+                g.drawString("WIN",50,200);         
         Scoops.Draw(g);
                 g.drawImage(Grass,Window.getX(0),Window.getY(0),
                 Window.getWidth2(),Window.getHeight2(),this);
@@ -162,10 +198,13 @@ public class Mancala extends JFrame implements Runnable {
             
             g.setColor(new Color(98,53,18));
 
+<<<<<<< HEAD
             g.fillRect(Window.getX(230),Window.getY(75)+Window.getHeight2()/3,
                 Window.getWidth2()/2,Window.getHeight2()/9);
 
 
+=======
+>>>>>>> 9db1122a8d024b5ebc9d69e292302c6406ff558f
             g.fillRect(Window.getX(315),Window.getY(75)+Window.getHeight2()/3,
                 Window.getWidth2()/4+50,Window.getHeight2()/10);
 
@@ -182,6 +221,8 @@ public class Mancala extends JFrame implements Runnable {
             g.drawString("WIN",50,200);         
         }
         gOld.drawImage(image, 0, 0, null);
+    }
+        }
     }
 ////////////////////////////////////////////////////////////////////////////
 // needed for     implement runnable
--- src/mancala/Marbles.java
+++ src/mancala/Marbles.java
@@ -12,14 +12,19 @@ public class Marbles {
     }
     
     public void draw(Graphics2D g,int row,int column,int xdelta,int ydelta) {
-        
+        int value = 0;
         g.setColor(color); 
         g.fillOval(Window.getX(column*xdelta),Window.getY(row*ydelta),xdelta,ydelta);   
+<<<<<<< HEAD
         g.setColor(Color.white);
         g.setFont(new Font("Arial",Font.BOLD,45));
         g.drawString("" + value,300,175); 
                 
 
+=======
+        g.setFont(new Font("Arial",Font.PLAIN,30));
+        g.drawString("" + value,Window.getX(column*xdelta+34),Window.getY(row*ydelta+50));            
+>>>>>>> 9db1122a8d024b5ebc9d69e292302c6406ff558f
     }    
     
 
