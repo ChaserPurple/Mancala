ea1f517f431a7162f723a0f64ac452e89a109841
ashcutie34 <44879310+ashcutie34@users.noreply.github.com>
2018-11-13 13:03:05 -0800

    Update src/mancala/Scoops.java

--- src/mancala/Scoops.java
+++ src/mancala/Scoops.java
@@ -83,16 +83,18 @@ public class Scoops {
         if (Player.getCurrentPlayer()==Player.getPlayer1()){
                 for (int zx = currCol;zx<board[currRow][currCol].getVal()+1;zx++)
                 {
-                    if (zx>0 && zx<7){
+                    if (zx>0 && zx<7 && currRow ==2){
                         if (board[currRow][zx] != null)
                         board[currRow][zx].addVal();
+                        
                     }
                 }
+
             }
             else {
                 for (int zx = currCol;zx<board[currRow][currCol].getVal()+1;zx--)
                 {
-                    if (zx>0 && zx<7){
+                    if (zx>0 && zx<7 && currRow ==1){
                         if (board[currRow][zx] != null)
                         board[currRow][zx].addVal();
                     }
